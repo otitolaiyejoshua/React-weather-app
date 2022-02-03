78a3c688863036906570f47ef21416c78c463138
otitolaiyejoshua <72415298+otitolaiyejoshua@users.noreply.github.com>
2022-02-03 03:50:23 +0100

    Create index.js

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '4c35ca129b3d80f4303a4632af8adfcf';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export const fetchWeather = (city) => {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
